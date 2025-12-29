71d5cc347defd525a2e8a921dc04f5fea972052a
baokimho <kim.ho@edu.turkuamk.fi>
2025-12-29 09:39:33 +0700

    ex 2.2

--- part2/courseinfo/src/components/Total.jsx
+++ part2/courseinfo/src/components/Total.jsx
@@ -2,7 +2,7 @@ const Total = ({parts}) => {
     
     return(
         <div>
-            <p>Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
+            <b>Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}</b>
         </div>
     )   
 }
