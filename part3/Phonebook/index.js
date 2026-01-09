ed6341a600cf1d12c66a701b83de425c375eba1f
baokimho <kim.ho@edu.turkuamk.fi>
2026-01-09 17:25:34 +0700

    ex 3.1 3.2

---
+++ part3/Phonebook/index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+
+const data = 
+[
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(data)
+})
+
+app.get('/info', (req, res) => {
+    res.send(`Phonebook has info for ${data.length} people <br>${Date()}`)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
