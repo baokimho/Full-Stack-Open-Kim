a85d1f0bf80ffb44e934a081854133e9f19a0886
baokimho <kim.ho@edu.turkuamk.fi>
2026-01-15 11:03:43 +0700

    uibuild

--- part3/Phonebook/index.js
+++ part3/Phonebook/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const path = require('path')
 
 let data = 
 [
@@ -33,7 +34,7 @@ morgan.token('body', (req) => {
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static('dist'))
+app.use(express.static(path.join(__dirname, 'dist')))
 // app.use(morgan("tiny"))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
